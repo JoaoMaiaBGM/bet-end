6ff1621677d4fc6e35aa17ea0f69d4ee1e1bea5d
João Maia <joaocarlosbgm@gmail.com>
2022-11-26 15:20:56 -0300

    feat: delete match

--- src/routes/matchRoutes.routes.ts
+++ src/routes/matchRoutes.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import matchCreateController from "../controllers/matches/matchCreate.controller";
+import matchDeleteController from "../controllers/matches/matchDelete.controller";
 import matchListController from "../controllers/matches/matchList.controller";
 import matchUpdateController from "../controllers/matches/matchUpdate.controller";
 
@@ -8,6 +9,6 @@ const matchRoutes = Router();
 matchRoutes.post("", matchCreateController);
 matchRoutes.get("", matchListController);
 matchRoutes.patch("/:id", matchUpdateController);
-matchRoutes.delete("/:id");
+matchRoutes.delete("/:id", matchDeleteController);
 
 export default matchRoutes;
