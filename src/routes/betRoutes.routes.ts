d1105531a754b904024181b9dd3e8f099d2d633b
João Maia <joaocarlosbgm@gmail.com>
2022-11-26 19:19:15 -0300

    feat: create bet

--- src/routes/betRoutes.routes.ts
+++ src/routes/betRoutes.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import betCreateController from "../controllers/bets/betCreate.controller";
 
 const betRoutes = Router();
 
-betRoutes.post("/");
+betRoutes.post("", betCreateController);
 betRoutes.get("/:id");
 betRoutes.patch("/:id");
 betRoutes.delete("/:id");
